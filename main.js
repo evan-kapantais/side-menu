325fb8ee2bac5cedc131365a1c49063f30bc78e3
evan-kapantais <evankapantais@gmail.com>
2019-04-02 19:15:04 +0200

    final product

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const btnSearch = document.querySelector("#btn-search");
 const btnRun = document.querySelector("#btn-run");
-
 const btnSearchClose = document.querySelector(".search-panel button");
 const btnRunClose = document.querySelector(".run-panel button");
-
 const searchPanel = document.querySelector(".search-panel");
 const runPanel = document.querySelector(".run-panel");
 
 btnSearch.addEventListener("click", () => handlePanels(searchPanel, "open"));
 btnRun.addEventListener("click", () => handlePanels(runPanel, "open"));
-
 btnSearchClose.addEventListener("click", () => handlePanels(searchPanel, "close"));
 btnRunClose.addEventListener("click", () => handlePanels(runPanel, "close"));
 
 function handlePanels(panelClass, action) {
 
-  searchPanel.style.left = "-30%";
-  runPanel.style.left = "-30%";
+  searchPanel.style.left = "-20%";
+  runPanel.style.left = "-20%";
 
   if (action == "open") {
     panelClass.style.left = "0%";
+    
+    document.onclick = function(e) {
+      if (e.target.type !== "button" && !e.target.classList.contains("panel")) {
+        searchPanel.style.left = "-20%";
+        runPanel.style.left = "-20%";
+      }
+    }
   } else {
-    panelClass.style.left = "-30%";
+    panelClass.style.left = "-20%";
   }
 }
 
-document.onclick = function(e) {
-  if (e.target.classList.contains("search-panel") || e.target.classList.contains("run-panel")) {
-    searchPanel.style.left = "-30%";
-    runPanel.style.left = "-30%";
-  }
-}
